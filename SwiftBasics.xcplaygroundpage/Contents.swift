3b5cd13a3e2c2e9846db035b1cce9ca45735316a
Колодницкий Илья <80146551+KolodnitskyIlya@users.noreply.github.com>
2024-03-28 23:44:19 +0600

    Swift

---
+++ SwiftBasics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+var greeting = "Hello, playground"
+
+print(greeting)
+print(greeting)
+print(greeting)
+
+// This is a single-line comment
+
+// These are multiple single-line comments
+// This is a comment
+// and this is some more info
+// (CMD + /)
+
+/*
+ This is a multi-line comment
+ that goes to
+ multiple lines!
+ */
+
+
+// Naming Conventions
+
+// Camel Case - CORRECT!
+// The first word is lowercased and then the first character in every following word is uppercased.
+
+let firstGreeting = "Hello, world!"
+let thisIsMyFirstGreeting = "Hello, world!"
+
+// Snake Case - wrong
+
+let this_is_what_snake_case_looks_like = "Hello, world!"
+
+// Pascal Case - wrong
+
+let ThisIsWhatPascalCaseLooksLike = "Hello, world!"
