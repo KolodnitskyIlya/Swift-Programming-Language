7fa37df485cbed49f8fc779735039b8047ad3c8f
Колодницкий Илья <80146551+KolodnitskyIlya@users.noreply.github.com>
2024-04-03 09:26:40 +0600

    Protocols in SwiftUI

---
+++ ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  MyTestApp
+//
+//  Created by macbook on 03.04.2024.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+
