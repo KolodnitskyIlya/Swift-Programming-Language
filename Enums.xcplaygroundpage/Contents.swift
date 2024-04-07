ecde95b686d3b1e2ada8211a1495c46d0a88092c
Колодницкий Илья <80146551+KolodnitskyIlya@users.noreply.github.com>
2024-04-08 00:39:13 +0600

    updating enums

--- Enums.xcplaygroundpage/Contents.swift
+++ Enums.xcplaygroundpage/Contents.swift
@@ -49,3 +49,70 @@ var car3 = CarModel(brand: .toyota, model: "Camry")
 var fordBrand: CardBrandOption = .ford
 
 print(fordBrand.title)
+
+
+// ----------------------------------------------------------------
+
+
+let result = "failure"
+let result2 = "failed"
+let result3 = "fail"
+
+enum Result {
+    case success
+    case failure
+}
+
+var result4 = Result.failure
+
+result4 = .success
+
+
+let result5: Result
+
+result5 = .success
+
+
+
+enum Activity {
+    case dancing
+    case running(dectination: String)
+    case talking(topic: String)
+    case singing(volume: Int)
+}
+
+let currentActivity = Activity.dancing
+
+switch currentActivity {
+    
+case .dancing: print("Dance")
+case .running: print("Run")
+case .singing: print("Song")
+case .talking: print("Talk")
+    
+}
+
+let talking = Activity.talking(topic: "Football")
+
+
+enum Planet: Int {
+    case mercury = 1
+    case venus
+    case earth
+    case mars
+}
+
+let earth = Planet(rawValue: 3)
+
+
+enum Phone: String {
+    case Apple = "iPhone 15"
+    case Samsung = "Galaxy S20"
+    case Google = "Pixel 2"
+}
+
+var myPhone = Phone.Apple
+
+print(myPhone)
+print(myPhone.rawValue)
+
